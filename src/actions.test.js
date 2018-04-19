5f8705b03d40422056fe91d0e15cdffe552b6fcc
Benilyn <benilyn.gonzales@gmail.com>
2018-04-19 08:40:53 -0700

    test actions

---
+++ src/actions.test.js
@@ -0,0 +1,30 @@
+import {
+	GENERATE_AURAL_UPDATE, generateAuralUpdate, 
+	RESTART_GAME, restartGame, 
+	MAKE_GUESS, makeGuess
+} from './actions';
+
+describe('generateAuralUpdate', () => {
+	it('should return the action', () => {
+		const action = generateAuralUpdate();
+		expect(action.type).toEqual(GENERATE_AURAL_UPDATE);
+	}); 
+}); {/* generateAuralUpdate */}
+
+describe('restartGame', () => {
+	it('should return the action', () => {
+		const correctAnswer = 8;
+		const action = restartGame(correctAnswer);
+		expect(action.type).toEqual(RESTART_GAME);
+		expect(action.correctAnswer).toEqual(correctAnswer);
+	});
+}); {/* restartGame */}
+
+describe('makeGuess', () => {
+	it('should return the action', () => {
+		const guess = 25;
+		const action = makeGuess(guess);
+		expect(action.type).toEqual(MAKE_GUESS);
+		expect(action.guess).toEqual(guess);
+	});
+}); {/* makeGuess */}
