e132f45e8266d855439dfc470b6dec9e97ec49e7
Benilyn <benilyn.gonzales@gmail.com>
2018-04-22 00:14:05 -0700

    test for unknown action and when nothing passed

---
+++ src/reducers.test.js
@@ -0,0 +1,56 @@
+import {generateAuralUpdate, restartGame, makeGuess} from './actions';
+import reducer from './reducer';
+
+describe('reducer', () => {
+	let state = {
+	    guesses: [],
+	    feedback: 'Make your guess!',
+	    auralStatus: '',
+	    correctAnswer: Math.round(Math.random() * 100) + 1
+	};
+
+	it('should set the initial state when nothing is passed', () => {
+		const state = reducer(undefined, {type: '_UNKNOWN'});
+		expect(state).toEqual({
+			guesses: [],
+    		feedback: 'Make your guess!',
+    		auralStatus: '',
+    		correctAnswer: state.correctAnswer
+		});
+	}); {/* should set the initial state when nothing is passed */}
+
+	it('should return the current state on an unknown action', () => {
+        let currentState = {};
+        const state = reducer(currentState, {type: '__UNKNOWN'});
+        expect(state).toBe(currentState);
+    }); {/* should return the current state on an unknown action */}
+
+//    it('restart game', () => {
+//   	let state = {
+//    		guesses : [3,6,9],
+//    		feedback : 'cold',
+//    		correctAnswer : 54
+//    	};
+    	
+//    	state = reducer(state, {type: 'RESTART_GAME'});
+//    	expect(state/).toEqual({
+//			guesses: [],
+//   		feedback: 'Make your guess!',
+//    		auralStatus: '',
+//    		correctAnswer: state.correctAnswer
+//		});
+//		console.log(state.correctAnswer);
+//    }); {/* restart game*/}
+
+//    it('make guess', () => {
+//    	let state = {
+//    		guesses: [],
+//    		feedback: '',
+//    		correctAnswer: 54
+//    	}
+//    	state = reducer(state, makeGuess(32));
+//    	console.log(state.feedback);
+//    	expect(state.guess).toEqual([32]);
+//    	expect(state.feedback).toEqual(`You're Warm.`);
+//    }); {/* make guess */}
+});
